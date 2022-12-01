acda5195d73518b73982dfb392244ed2e98c93ed
robynroby <robinsonmwaura254@gmail.com>
2022-12-01 22:53:53 +0300

    handler to add new item and return specific item

--- main.go
+++ main.go
@@ -28,6 +28,38 @@ func getAlbums(c *gin.Context){
 
 func main() {
 	router := gin.Default()
-	router.GET("/albums" , getAlbums)
+	router.GET("/albums", getAlbums)
+	router.GET("/albums:id", getAlbumByID)
+	router.POST("/albums", postAlbums)
 	router.Run("localhost:8080")
 }
+
+// postAlbums adds an album from JSON received in the request body
+func postAlbums(c *gin.Context){
+	var newAlbum album
+
+// call BindJSON to bind the received Json to newAlbum
+if err := c.BindJSON(&newAlbum); err != nil {
+	return
+}
+
+// add the new albulm to the slice 
+albums = append(albums ,newAlbum)
+c.IndentedJSON(http.StatusCreated, newAlbum)
+}
+
+// getAlbumByID locates the album whose ID value matches the id
+// parameter sent by the client, then returns that album as a response.
+func getAlbumByID(c *gin.Context){
+	id := c.Param("id")
+
+	// loop over the list of albums looking for an album  whose id value matches the parameter
+	for _, a := range albums{
+		if a.ID == id{
+			c.IndentedJSON(http.StatusOK, a)
+			return
+		}
+	}
+	c.IndentedJSON(http.StatusNotFound, gin.H{"message":"albulm not found!!"})
+}
+
