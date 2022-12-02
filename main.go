3f518612acefed24690847135c20b1b1a60c2eec
robynroby <robinsonmwaura254@gmail.com>
2022-12-02 10:12:45 +0300

    api change

--- main.go
+++ main.go
@@ -1,17 +1,16 @@
 package main
 
-
-import(
-	"net/http"
+import (
 	"github.com/gin-gonic/gin"
+	"net/http"
 )
 
 // albulm represents data about a record albulm
 type album struct {
-	ID string `json:"id"`
-	Title string `json:"title"`
-	Artist string `json:"artist"`
-	Price float64 `json:"price"`
+	ID     string  `json:"id"`
+	Title  string  `json:"title"`
+	Artist string  `json:"artist"`
+	Price  float64 `json:"price"`
 }
 
 // albulm slice to seed record albulm data
@@ -22,7 +21,7 @@ var albums = []album{
 }
 
 // getAlbums responds with the list of all albums as jSON
-func getAlbums(c *gin.Context){
+func getAlbums(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, albums)
 }
 
@@ -35,31 +34,30 @@ func main() {
 }
 
 // postAlbums adds an album from JSON received in the request body
-func postAlbums(c *gin.Context){
+func postAlbums(c *gin.Context) {
 	var newAlbum album
 
-// call BindJSON to bind the received Json to newAlbum
-if err := c.BindJSON(&newAlbum); err != nil {
-	return
-}
+	// call BindJSON to bind the received Json to newAlbum
+	if err := c.BindJSON(&newAlbum); err != nil {
+		return
+	}
 
-// add the new albulm to the slice 
-albums = append(albums ,newAlbum)
-c.IndentedJSON(http.StatusCreated, newAlbum)
+	// add the new albulm to the slice
+	albums = append(albums, newAlbum)
+	c.IndentedJSON(http.StatusCreated, newAlbum)
 }
 
 // getAlbumByID locates the album whose ID value matches the id
 // parameter sent by the client, then returns that album as a response.
-func getAlbumByID(c *gin.Context){
+func getAlbumByID(c *gin.Context) {
 	id := c.Param("id")
 
 	// loop over the list of albums looking for an album  whose id value matches the parameter
-	for _, a := range albums{
-		if a.ID == id{
+	for _, a := range albums {
+		if a.ID == id {
 			c.IndentedJSON(http.StatusOK, a)
 			return
 		}
 	}
-	c.IndentedJSON(http.StatusNotFound, gin.H{"message":"albulm not found!!"})
+	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "albulm not found!!"})
 }
-
